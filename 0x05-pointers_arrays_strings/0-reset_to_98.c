828b158e5d00bb450727de92cbfea227b8815bec
Ojo olalekan <olalekanoj89@gmail.com>
2022-07-14 03:35:33 -0700

    Task 11

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  *
  * * reset_to_98 -> a function to update the reference of a pointer
@@ -8,9 +7,7 @@
  * * @n: pointer to n to be updated
  *
  * */
-
 void reset_to_98(int *n)
-
 {
 
 *n = 98;
